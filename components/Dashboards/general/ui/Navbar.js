c6bc716397acba65d8300b052753c2b8b21c2783
doruk <dorukbulutinfo@gmail.com>
2022-11-17 21:51:11 +0300

    responsive Navbar

--- components/Dashboards/general/ui/Navbar.js
+++ components/Dashboards/general/ui/Navbar.js
@@ -1,9 +1,66 @@
+import {useState } from 'react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 export default function Navbar () {
+    const [isOpen, setIsOpen] = useState(true);
+    const [sales, setSales ] = useState(false);
+    const toggleSales  = () => {
+      setSales(!sales);
+    } 
+    const toggleNav = () => {
+      setIsOpen(!isOpen);
+    }
     return (
-        <div>
-          neden
-        </div>
+      <>
+       <div className={isOpen ? "border border-r-2 border-solid absolute w-3/5 md:w-1/3 lg:w-1/5 h-screen bg-gray-100 z-40  p-5 translate-x-0 ease-in-out duration-300" :"fixed z-40 top-0 left-0  h-screen -translate-x-full ease-in-out duration-100 p-5"}>
+          <div className={!isOpen ? "p-10 absolute" : "hidden"}>
+            <svg onClick={toggleNav} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="hover:cursor-pointer stroke-gray-500 justify-self-end w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </div>
 
+          <div className={isOpen ? "flex flex-col space-y-10 items-start" : "hidden"}>
 
+            <div className="grid ease-in-out">
+              <svg onClick={toggleNav}  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="hover:cursor-pointer hover:bg-gray-300 stroke-gray-500 justify-self-end w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </div>
+
+            <div className='flex items-center space-x-5'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-sky-700 w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+              </svg>
+
+              <p className="font-poppins text-xs text-sky-700 tracking-widest">VNT METAL DYS</p>
+              <hr/>
+            </div>
+
+            
+
+            <div className='flex flex-col space-y-5 items-end '>
+                <div className='flex items-center space-x-5'>
+                  <svg  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="stroke-sky-700 w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <p className="font-poppins text-xs text-sky-700 tracking-widest">Sipariş Modülü</p>
+                  <ChevronDownIcon onClick={toggleSales} className={`-mr-1 ml-2 h-5 w-5 ${sales ? "rotate-180": ""} ease-in-out duration-300 hover:cursor-pointer`} aria-hidden="true" />
+                </div>
+
+                <div className={`space-y-6 tracking-widest font-poppins text-sm text-sky-600 ${sales? "visible" : "invisible"} transition ease-out delay-3000`}>
+                    
+                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Müşterilerim</p>
+                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Tekliflerim</p>
+                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Satış Formlarım</p>
+                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Iş Emirlerim</p>
+                    
+                    
+                </div>
+               
+            </div>
+            
+          </div>
+
+       </div>
+       </>
     );
 }
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,14 @@
 import Navbar from "../../components/Dashboards/general/ui/Navbar"
+import Footer from "../../components/base/Footer";
 export default function DashBoard () {
     return (
-        <div className="grid grid-rows-1 grid-cols-4">
-            <div className="col-span-1">
+        <div className="grid grid-cols-5">
+            <div className="">
                 <Navbar /> 
             </div>
 
-            <div className="col-span-3">
-                alalh
+            <div className="col-span-5 bg-gray-100 h-screen">
+                
             </div>
             
         </div>
