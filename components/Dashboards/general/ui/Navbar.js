32191aea475ef797e037fbefdce47f78fd9c177d
doruk <dorukbulutinfo@gmail.com>
2022-11-18 17:23:50 +0300

    Finish Sales Module Navbar Layout

--- components/Dashboards/general/ui/Navbar.js
+++ components/Dashboards/general/ui/Navbar.js
@@ -3,6 +3,11 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 export default function Navbar () {
     const [isOpen, setIsOpen] = useState(true);
     const [sales, setSales ] = useState(false);
+    const [quotation, setQuotation] = useState(false);
+    
+    const toggleQuotation = () => {
+      setQuotation(!quotation);
+    }
     const toggleSales  = () => {
       setSales(!sales);
     } 
@@ -26,30 +31,39 @@ export default function Navbar () {
               </svg>
             </div>
 
-            <div className='flex items-center space-x-5'>
+            <div className='flex items-center space-x-5 border-solid border-b-2 p-2'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-sky-700 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
               </svg>
 
-              <p className="font-poppins text-xs text-sky-700 tracking-widest">VNT METAL DYS</p>
+              <p className="font-poppins text-xs lg:text-lg md:text-lg text-sky-700 tracking-widest">VNT METAL DYS</p>
               <hr/>
             </div>
 
             
-
-            <div className='flex flex-col space-y-5 items-end '>
+            {/*Sales navigation*/}
+            <div className='flex flex-col items-end pb-2 space-y-2'>
                 <div className='flex items-center space-x-5'>
-                  <svg  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="stroke-sky-700 w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  <svg  fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="stroke-sky-700 w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  <p className="font-poppins text-xs text-sky-700 tracking-widest">Sipariş Modülü</p>
+                  <p onClick={toggleSales} className="hover:cursor-pointer font-poppins text-xs text-sky-700 tracking-widest">Sipariş Modülü</p>
                   <ChevronDownIcon onClick={toggleSales} className={`-mr-1 ml-2 h-5 w-5 ${sales ? "rotate-180": ""} ease-in-out duration-300 hover:cursor-pointer`} aria-hidden="true" />
                 </div>
 
-                <div className={`space-y-6 tracking-widest font-poppins text-sm text-sky-600 ${sales? "visible" : "invisible"} transition ease-out delay-3000`}>
+                <div className={`flex flex-col space-y-4 tracking-widest font-poppins lg:text-xs text-xs text-sky-600 ${sales? "visible  scale-100 border-solid border-l-2" : "invisible transform scale-0 h-0"} transition duration-300 ease-in-out origin-top`}>
                     
-                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Müşterilerim</p>
-                      <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Tekliflerim</p>
+                      <p className=" hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md">Müşterilerim</p>
+                      <div className="flex flex-col lg:text-xs text-xs mb-10 ">
+                        <div className='flex items-center w-full'>
+                          <p className="hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md">Tekliflerim</p>
+                          <ChevronDownIcon onClick={toggleQuotation} className={`-mr-1 ml-2 h-5 w-5 ${quotation ? "rotate-180": "rotate-0"} ease-in-out duration-300 hover:cursor-pointer`} aria-hidden="true" />
+                        </div>
+                        <div className={`flex flex-col w-full justify-self-end items-end space-y-2 ${quotation? "visible  scale-100 " : "invisible transform scale-0 h-0"} transition duration-300 ease-in-out origin-top`}>
+                          <p className='pl-10 hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Teklif Hazırlama</p>
+                          <p className='pl-10 hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Teklif Formlarım</p>
+                        </div>
+                      </div>
                       <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Satış Formlarım</p>
                       <p className='hover:bg-gray-200 hover:cursor-pointer p-2 hover:rounded-md'>Iş Emirlerim</p>
                     
@@ -57,6 +71,8 @@ export default function Navbar () {
                 </div>
                
             </div>
+
+            
             
           </div>
 
