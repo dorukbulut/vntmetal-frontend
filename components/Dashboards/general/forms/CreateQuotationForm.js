a8f1622dccc9c389b43e3790727e9902e67c7b03
doruk <dorukbulutinfo@gmail.com>
2022-12-20 18:40:06 +0300

    Finish Quotation Form Fields

--- components/Dashboards/general/forms/CreateQuotationForm.js
+++ components/Dashboards/general/forms/CreateQuotationForm.js
@@ -5,6 +5,74 @@ import Dropdown from "./Dropdown";
 import SetItem from "./SetQuotationItem";
 import { useStepContext } from "@mui/material";
 
+const INCOTERMS = [
+  {
+    key : "VNTFT",
+    value : "VNTFT",
+
+  },
+  {
+    key : "MT",
+    value : "MT",
+
+  },
+  {
+    key : "EXW",
+    value : "EXW",
+
+  },
+  {
+    key : "FCA",
+    value : "FCA",
+
+  },
+  {
+    key : "FAS",
+    value : "FAS",
+
+  },
+  {
+    key : "FOB",
+    value : "FOB",
+
+  },
+  {
+    key : "CFR",
+    value : "CFR",
+
+  },
+  {
+    key : "CIF",
+    value : "CIF",
+
+  },
+  {
+    key : "CPT",
+    value : "CPT",
+
+  },
+  {
+    key : "CIP",
+    value : "CIP",
+
+  },
+  {
+    key : "DAP",
+    value : "DAP",
+
+  },
+  {
+    key : "DPU",
+    value : "DPU",
+
+  },
+  {
+    key : "DDP",
+    value : "DDP",
+
+  },
+]
+
 
 export default function CreateQuotationForm({customers}) {
   const CUSTOMER = customers.map((customer) => {
@@ -21,6 +89,9 @@ export default function CreateQuotationForm({customers}) {
     options : {
       Customer_ID : '',
       customerInquiryNum : '',
+    },
+    delivery_type : {
+       name : '',
     }
   }); 
   const router = useRouter();
@@ -176,6 +247,215 @@ export default function CreateQuotationForm({customers}) {
                 </div>
               </div>
 
+              {/*Tax Info*/}
+              <div className="m-0 space-y-7 lg:flex lg:flex-col lg:items-center">
+                <div className="space-y-2 lg:w-1/2">
+                  <p className="text-center font-poppins text-gray-500 font-medium text-sm ">
+                    Incoterm Detayları
+                  </p>
+                  <hr />
+                </div>
+
+                <div className="space-y-5 lg:grid lg:grid-cols-3 lg:place-items-center">
+                  <div className="flex flex-col space-y-3 ">
+                                <label
+                                  htmlFor="small-input"
+                                  className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+                                >
+                                  Incoterm Tipi *
+                                </label>
+                                <Dropdown
+                                  label="Incoterm"
+                                  field="delivery_type"
+                                  area="name"
+                                  items={INCOTERMS}
+                                  fields={fields}
+                                  handleChange={handleChange}
+                                />
+                  </div>
+
+                  <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Paketleme *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Yükleme Ücreti *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Liman veya Belirlenen Yerde teslim *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                İhracat Prosedürleri *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Çıkış terminali ücretleri *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Araca Yükleme *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Taşıma masrafları *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Sigorta *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                Varış Terminali Ücretleri *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="small-input"
+                className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
+              >
+                İthalat Prosedürleri *
+              </label>
+              <input
+                type="number"
+                step={"any"}
+                
+                className="invalid:border-red-500 valid:border-green-500 pl-5 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
+                placeholder=""
+                required
+                onChange={(e) => handleChange("calc_raw", "euro", e)}
+              />
+            </div>
+
+                </div>
+              </div>
+
               {/*Address Info*/}
               <div className=" m-0 lg:flex  space-y-5 lg:flex-col lg:items-center">
                 <div className="space-y-2 lg:w-1/2">
@@ -186,6 +466,8 @@ export default function CreateQuotationForm({customers}) {
                 </div>
 
                 <div className="space-y-2 lg:grid lg:grid-cols-1 lg:w-1/3 lg:items-end lg:gap-3 ">
+                
+                  
                   <div className="flex flex-col ">
                     <label
                       htmlFor="small-input"
@@ -202,13 +484,17 @@ export default function CreateQuotationForm({customers}) {
                       }
                     />
                   </div>
+                
+                 
 
+                  <div className="flex flex-col"></div>
+                  
                   <div className="flex flex-col ">
                     <label
                       htmlFor="small-input"
                       className="block mb-2 text-sm font-medium font-poppins italic text-sky-600 text-gray-900 dark:text-gray-300"
                     >
-                      Incoterm Tipi
+                      Incoterm Açıklama
                     </label>
                     <textarea
                       type="text"
