0e5635330e505a4dc3bfe4e7fef8e37310c457cf
doruk <dorukbulutinfo@gmail.com>
2023-01-02 21:49:59 +0300

    Quotation Item Filter

--- pages/admin/quotation/make.js
+++ pages/admin/quotation/make.js
@@ -6,13 +6,75 @@ import CreateMake from "../../../components/Dashboards/general/forms/CreateaMake
 import CreateAnalyze from "../../../components/Dashboards/general/forms/CreateAnalyze";
 import axios from "axios";
 import UpdateCustomer from "../../../components/Dashboards/general/forms/UpdateCustomer";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { useRouter } from "next/router";
-import usePagination from "../../../components/Dashboards/general/ui/Pagination";
-import Pagination from "@mui/material/Pagination";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import UpdateMake from "../../../components/Dashboards/general/forms/UpdateMake";
 
 export default function quotationMake({analyzes, customers, items}) {
+  const router = useRouter();
+  const [filters, setFilters] = useState();
+
+  const [quotItems, setquotItems] = useState(items.rows);
+
+  const [page, setPage] = useState(1);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+  useEffect(() => {
+    axios({
+      method : "GET",
+      url : `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-items/get-page/${parseInt(page) - 1}`,
+      withCredentials : true
+    })
+    .then(res => {
+      if (res.status === 200) {
+        setquotItems(res.data.rows);
+      }
+    })
+    .catch(err => console.log(err));
+  } , [page])
+
+  useEffect(() => {
+    console.log(filters);
+    
+    if(filters) {
+      axios({
+        method : "GET",
+        url : `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-items/filter`,
+        params : {
+          account : filters.account_id !== undefined && filters.account_id !== ''  ? filters.account_id : undefined,
+          type  :  filters.type !== undefined && filters.type !== '' ? filters.type.replaceAll(" ", "+") : undefined,
+          date : filters.date !== undefined && filters.date !== ''  ? filters.date : undefined,
+        }
+  
+      })
+      .then(res => {
+        if (res.status === 200) {
+          setquotItems(res.data.rows);
+        }
+      })
+      .catch(err => {
+        router.reload(window.location.pathname);
+      });
+    }
+    
+  }, [filters]) 
+  
+  
+  
+
+  const handleFilters = (field, e) => {
+    setFilters(old => {
+      return {
+        ...old,
+        [field] : e.target.value
+      }
+    });
+    console.log(filters);
+    router.replace(router.asPath);
+  };
   return (
     <div className="">
       <Navbar />
@@ -31,24 +93,11 @@ export default function quotationMake({analyzes, customers, items}) {
           <div className="flex space-x-20">
             <div className="flex flex-col space-y-3">
               <p className="text-xs font-poppins font-medium italic text-sky-700">
-                Ürün
-              </p>
-              <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
-                <input
-                  type="text"
-                  className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
-                  placeholder="Ürün Ara..."
-                  onChange={(e) => handleFilters("account_title", e)}
-                />
-              </div>
-            </div>
-            <div className="flex flex-col space-y-3">
-              <p className="text-xs font-poppins font-medium italic text-sky-700">
                 Cari Kod
               </p>
               <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
                 <input
-                  type="text"
+                  type="number"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Cari Kod Ara..."
                   onChange={(e) => handleFilters("account_id", e)}
@@ -65,7 +114,7 @@ export default function quotationMake({analyzes, customers, items}) {
                   type="date"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Tarih Ara..."
-                  onChange={(e) => handleFilters("customer_country", e)}
+                  onChange={(e) => handleFilters("date", e)}
                 />
               </div>
             </div>
@@ -74,7 +123,7 @@ export default function quotationMake({analyzes, customers, items}) {
 
         <div className="w-full bg-gray-100 ">
           <div className="relative overflow-x-auto shadow-md  sm:rounded-lg">
-            {items.length !== 0 ? (
+            {quotItems.length !== 0 ? (
               <table className="w-full text-sm text-left text-gray-500 ">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -91,13 +140,16 @@ export default function quotationMake({analyzes, customers, items}) {
                       ADET
                     </th>
                     <th scope="col" className="px-6 py-3">
+                      TARİH
+                    </th>
+                    <th scope="col" className="px-6 py-3">
                       <span className="sr-only">Düzenle</span>
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                 {
-                   items.map((item, index) =>  {
+                   quotItems.map((item, index) =>  {
                     return (<tr
                           key={index}
                           className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
@@ -120,6 +172,9 @@ export default function quotationMake({analyzes, customers, items}) {
                           <td className="px-6 py-4">
                             {item.unit_frequence}
                           </td>
+                          <td className="px-6 py-4">
+                            {item.createdAt}
+                          </td>
                           <td className="px-6 py-4 text-right">
                           <UpdateMake analyzes={analyzes} customers={customers} item={item}/>
                           </td>
@@ -141,7 +196,9 @@ export default function quotationMake({analyzes, customers, items}) {
         
       </div>
       <div className="grid place-items-center mb-10">
-        
+      {quotItems.length !==0 ? <Stack spacing={2}>
+        <Pagination count={Math.ceil(items.count / 6)} page={page} onChange={handlePageChange} />
+    </Stack> : <div></div>}
       </div>
       <Footer />
     </div>
@@ -158,7 +215,7 @@ export async function getServerSideProps(context) {
       `${process.env.NEXT_PUBLIC_BACKEND}/api/customer/all`
     );
     const res3 = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-items/all`
+      `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-items/get-page/0`
     )
     
     if (res.status === 200 && res2.status === 200 && res3.status === 200) {
