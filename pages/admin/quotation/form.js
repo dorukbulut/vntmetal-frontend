b1634fb40922ebbd8ec54db3de2457cde5def877
doruk <dorukbulutinfo@gmail.com>
2023-01-03 12:09:28 +0300

    page,filter

--- pages/admin/quotation/form.js
+++ pages/admin/quotation/form.js
@@ -3,10 +3,10 @@ import ProfileBar from "../../../components/Dashboards/general/ui/ProfileBar";
 import BreadCrumbs from "../../../components/Dashboards/general/ui/BreadCrumbs";
 import Footer from "../../../components/base/Footer";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import usePagination from "../../../components/Dashboards/general/ui/Pagination";
-import Pagination from "@mui/material/Pagination";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import CreateQuotationForm from "../../../components/Dashboards/general/forms/CreateQuotationForm";
 import UpdateQuotationForm from "../../../components/Dashboards/general/forms/UpdateQuotationForm";
 
@@ -25,7 +25,70 @@ export default function quotationMake({customers, forms}) {
         a.click();
       });
     });
-  } 
+  }
+  const router = useRouter();
+  const [filters, setFilters] = useState();
+
+  const [formItems, setformItems] = useState(forms.rows);
+
+  const [page, setPage] = useState(1);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+  useEffect(() => {
+    axios({
+      method : "GET",
+      url : `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-form/get-page/${parseInt(page) - 1}`,
+      withCredentials : true
+    })
+    .then(res => {
+      if (res.status === 200) {
+        setformItems(res.data.rows);
+      }
+    })
+    .catch(err => console.log(err));
+  } , [page])
+
+  useEffect(() => {
+    console.log(filters);
+    
+    if(filters) {
+      axios({
+        method : "GET",
+        url : `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-form/filter`,
+        params : {
+          account : filters.account_id !== undefined && filters.account_id !== ''  ? filters.account_id : undefined,
+          reference  :  filters.reference !== undefined && filters.reference !== '' ? filters.reference.replaceAll(" ", "+") : undefined,
+          date : filters.date !== undefined && filters.date !== ''  ? filters.date : undefined,
+          customer  :  filters.customer !== undefined && filters.customer !== '' ? filters.customer.replaceAll(" ", "+") : undefined,
+        }
+  
+      })
+      .then(res => {
+        if (res.status === 200) {
+          setformItems(res.data.rows);
+        }
+      })
+      .catch(err => {
+        router.reload(window.location.pathname);
+      });
+    }
+    
+  }, [filters]) 
+  
+  
+  
+
+  const handleFilters = (field, e) => {
+    setFilters(old => {
+      return {
+        ...old,
+        [field] : e.target.value
+      }
+    });
+    console.log(filters);
+    router.replace(router.asPath);
+  };
   return (
     <div className="">
       <Navbar />
@@ -50,7 +113,7 @@ export default function quotationMake({customers, forms}) {
                   type="text"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Ürün Ara..."
-                  onChange={(e) => handleFilters("account_title", e)}
+                  onChange={(e) => handleFilters("reference", e)}
                 />
               </div>
             </div>
@@ -60,7 +123,7 @@ export default function quotationMake({customers, forms}) {
               </p>
               <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
                 <input
-                  type="text"
+                  type="number"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Cari Kod Ara..."
                   onChange={(e) => handleFilters("account_id", e)}
@@ -77,7 +140,7 @@ export default function quotationMake({customers, forms}) {
                   type="text"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Cari Kod Ara..."
-                  onChange={(e) => handleFilters("account_id", e)}
+                  onChange={(e) => handleFilters("customer", e)}
                 />
               </div>
             </div>
@@ -91,7 +154,7 @@ export default function quotationMake({customers, forms}) {
                   type="date"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Tarih Ara..."
-                  onChange={(e) => handleFilters("customer_country", e)}
+                  onChange={(e) => handleFilters("date", e)}
                 />
               </div>
             </div>
@@ -100,7 +163,7 @@ export default function quotationMake({customers, forms}) {
 
         <div className="w-full bg-gray-100 ">
           <div className="relative overflow-x-auto shadow-md  sm:rounded-lg">
-            {forms.length !== 0 ? (
+            {formItems.length !== 0 ? (
               <table className="w-full text-sm text-left text-gray-500 ">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -130,7 +193,7 @@ export default function quotationMake({customers, forms}) {
                 </thead>
                 <tbody>
                 {
-                   forms.map((item, index) =>  {
+                   formItems.map((item, index) =>  {
                     return (<tr
                           key={index}
                           className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
@@ -178,7 +241,9 @@ export default function quotationMake({customers, forms}) {
         
       </div>
       <div className="grid place-items-center mb-10">
-        
+      {formItems.length !==0 ? <Stack spacing={2}>
+        <Pagination count={Math.ceil(forms.count / 6)} page={page} onChange={handlePageChange} />
+    </Stack> : <div></div>}
       </div>
       <Footer />
     </div>
@@ -193,7 +258,7 @@ export async function getServerSideProps(context) {
       `${process.env.NEXT_PUBLIC_BACKEND}/api/customer/all`
     );
     const r3 = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-form/all`
+      `${process.env.NEXT_PUBLIC_BACKEND}/api/quotation-form/get-page/0`
     )
     console.log(r3)
     if (res2.status === 200 && r3.status === 200 ) {
