4be97e498e514ec8fefedd20cdfb09483b2b11c7
doruk <dorukbulutinfo@gmail.com>
2023-01-03 13:49:29 +0300

    sale confiration paging and filtering

--- pages/admin/sale-confirmation.js
+++ pages/admin/sale-confirmation.js
@@ -3,12 +3,12 @@ import ProfileBar from "../../components/Dashboards/general/ui/ProfileBar";
 import BreadCrumbs from "../../components/Dashboards/general/ui/BreadCrumbs";
 import Footer from "../../components/base/Footer";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import usePagination from "../../components/Dashboards/general/ui/Pagination";
-import Pagination from "@mui/material/Pagination";
 import CreateConfirmationForm from "../../components/Dashboards/general/forms/CreateConfirmation";
 import UpdateConfirmationForm from "../../components/Dashboards/general/forms/UpdateConfirmation";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 export default function quotationMake({customers, confirmations}) {
   const generate = (e) => {
@@ -25,7 +25,69 @@ export default function quotationMake({customers, confirmations}) {
         a.click();
       });
     });
-  } 
+  }
+  const router = useRouter();
+  const [filters, setFilters] = useState();
+
+  const [formItems, setformItems] = useState(confirmations.rows);
+
+  const [page, setPage] = useState(1);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+  useEffect(() => {
+    axios({
+      method : "GET",
+      url : `${process.env.NEXT_PUBLIC_BACKEND}/api/sale-confirmation/get-page/${parseInt(page) - 1}`,
+      withCredentials : true
+    })
+    .then(res => {
+      if (res.status === 200) {
+        setformItems(res.data.rows);
+      }
+    })
+    .catch(err => console.log(err));
+  } , [page])
+
+  useEffect(() => {
+    
+    if(filters) {
+      axios({
+        method : "GET",
+        url : `${process.env.NEXT_PUBLIC_BACKEND}/api/sale-confirmation/filter`,
+        params : {
+          account : filters.account_id !== undefined && filters.account_id !== ''  ? filters.account_id : undefined,
+          saleReference  :  filters.saleReference !== undefined && filters.saleReference !== '' ? filters.saleReference.replaceAll(" ", "+") : undefined,
+          date : filters.date !== undefined && filters.date !== ''  ? filters.date : undefined,
+          quotReference  :  filters.quotReference !== undefined && filters.quotReference !== '' ? filters.quotReference.replaceAll(" ", "+") : undefined,
+        }
+  
+      })
+      .then(res => {
+        if (res.status === 200) {
+          setformItems(res.data.rows);
+        }
+      })
+      .catch(err => {
+        router.reload(window.location.pathname);
+      });
+    }
+    
+  }, [filters]) 
+  
+  
+  
+
+  const handleFilters = (field, e) => {
+    setFilters(old => {
+      return {
+        ...old,
+        [field] : e.target.value
+      }
+    });
+    console.log(filters);
+    router.replace(router.asPath);
+  };
   return (
     <div className="">
       <Navbar />
@@ -50,7 +112,7 @@ export default function quotationMake({customers, confirmations}) {
                   type="text"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Ürün Ara..."
-                  onChange={(e) => handleFilters("account_title", e)}
+                  onChange={(e) => handleFilters("saleReference", e)}
                 />
               </div>
             </div>
@@ -64,7 +126,7 @@ export default function quotationMake({customers, confirmations}) {
                   type="text"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Ürün Ara..."
-                  onChange={(e) => handleFilters("account_title", e)}
+                  onChange={(e) => handleFilters("quotReference", e)}
                 />
               </div>
             </div>
@@ -74,7 +136,7 @@ export default function quotationMake({customers, confirmations}) {
               </p>
               <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
                 <input
-                  type="text"
+                  type="number"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Cari Kod Ara..."
                   onChange={(e) => handleFilters("account_id", e)}
@@ -90,7 +152,7 @@ export default function quotationMake({customers, confirmations}) {
                   type="date"
                   className="pl-9 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-sky-600 focus:outline-none focus:transition-shadow"
                   placeholder="Tarih Ara..."
-                  onChange={(e) => handleFilters("customer_country", e)}
+                  onChange={(e) => handleFilters("date", e)}
                 />
               </div>
             </div>
@@ -99,7 +161,7 @@ export default function quotationMake({customers, confirmations}) {
 
         <div className="w-full bg-gray-100 ">
           <div className="relative overflow-x-auto shadow-md  sm:rounded-lg">
-            {confirmations.length !== 0 ? (
+            {formItems.length !== 0 ? (
               <table className="w-full text-sm text-left text-gray-500 ">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -132,7 +194,7 @@ export default function quotationMake({customers, confirmations}) {
                 </thead>
                 <tbody>
                 {
-                   confirmations.map((item, index) =>  {
+                   formItems.map((item, index) =>  {
                     return (<tr
                           key={index}
                           className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
@@ -184,7 +246,9 @@ export default function quotationMake({customers, confirmations}) {
         
       </div>
       <div className="grid place-items-center mb-10">
-        
+      {formItems.length !==0 ? <Stack spacing={2}>
+        <Pagination count={Math.ceil(confirmations.count / 6)} page={page} onChange={handlePageChange} />
+    </Stack> : <div></div>}
       </div>
       <Footer />
     </div>
@@ -199,7 +263,7 @@ export async function getServerSideProps(context) {
     );
 
     const res3 = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND}/api/sale-confirmation/all`
+      `${process.env.NEXT_PUBLIC_BACKEND}/api/sale-confirmation/get-page/0`
     );
     if (res2.status === 200 && res3.status === 200 ) {
       return {
