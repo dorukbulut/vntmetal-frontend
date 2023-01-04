7131a51cefb34d12007706313409eac2935c2d05
doruk <dorukbulutinfo@gmail.com>
2023-01-04 10:04:02 +0300

    bug fix

--- components/Dashboards/general/forms/Table/index.js
+++ components/Dashboards/general/forms/Table/index.js
@@ -25,10 +25,11 @@ const Table = ({ columns, data, updateMyData, skipPageReset }) => {
             {...getTableProps()}
           >
             <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
+              {headerGroups.map((headerGroup, index) => (
+                <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column, index) => (
                     <th
+                    key={index}
                       className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                       {...column.getHeaderProps()}
                     >
@@ -45,10 +46,11 @@ const Table = ({ columns, data, updateMyData, skipPageReset }) => {
               {rows.map((row, i) => {
                 prepareRow(row);
                 return (
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => {
+                  <tr key={i} {...row.getRowProps()}>
+                    {row.cells.map((cell, i) => {
                       return (
                         <td
+                        key={i}
                           className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900"
                           {...cell.getCellProps()}
                         >
