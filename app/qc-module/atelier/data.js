956346166af0c02d99cd20dbd946585bf50d6824
Doruk Bulut <dorukbulutinfo@gmail.com>
2023-06-05 17:32:11 +0300

    QC-Module Finish

---
+++ app/qc-module/atelier/data.js
@@ -0,0 +1,7 @@
+export const columns = [
+    { id: "reference", label: "Döküm No.", minWidth: 170 },
+    { id: "step", label: "Sıra", minWidth: 170 },
+    { id: "isQC", label: "Kalite Durumu", minWidth: 170 },
+    { id: "options", label: "İşlem", minWidth: 170 },
+
+]
--- services/QCService/index.js
+++ services/QCService/index.js
@@ -18,6 +18,24 @@ class QCService {
     async setQC(product_id, qcValue) {
         return axios.post("/api/qc-production/set-qc", {product_id, qcValue});
     }
+
+    async getPageAtelier(pageNum) {
+        return axios.get(`/api/qc-atelier/get-page/${pageNum}`);
+    }
+
+    async getDefaultDataAtelier() {
+        return this.getPage(0);
+    }
+
+    async getFilteredDataAtelier(filters) {
+        return axios.get("/api/qc-atelier/filter", {
+            params: { ...filters },
+        });
+    }
+
+    async setQCAtelier(atelier_id, qcValue) {
+        return axios.post("/api/qc-atelier/set-qc", {atelier_id, qcValue});
+    }
 }
 
 
